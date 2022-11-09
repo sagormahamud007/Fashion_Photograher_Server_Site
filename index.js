7161e385f5daab0df0cfda1626d0108e611feb7f
Shagor <mdshagormahamud028@gmail.com>
2022-11-09 14:57:00 +0600

    get the all data show, from mongodb database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const colors = require('colors');
 require('dotenv').config();
@@ -8,6 +9,29 @@ const app = express()
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uuzniqz.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('tourServices').collection('services')
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
 
 app.get('/', (req, res) => {
     res.send('fashion photography is running')
